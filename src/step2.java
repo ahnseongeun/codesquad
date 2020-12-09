7b4f85ce713574d776f39fa8d326ede4500062a6
ahnseongeun <ast3138@naver.com>
2020-12-10 01:48:31 +0900

    cube_display 추가

--- src/step2.java
+++ src/step2.java
@@ -1,5 +1,23 @@
-public class step2 {
-    public static void main(String[] args) throws Exception{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class step2 {
+    static void cube_display(char[][] cube){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++)
+                sb.append(cube[i][j]+" ");
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st=new StringTokenizer(input.readLine());
+        char cube[][]={{'R','R','W'},
+                       {'G','C','W'},
+                       {'G','B','B'}};
+        cube_display(cube);
     }
 }
