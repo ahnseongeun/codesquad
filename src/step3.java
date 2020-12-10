4239be12096352ba9577364da1d0c6807808388a
seongeun ahn <ast3138@hanmail.net>
2020-12-10 10:51:01 +0900

    step3 Test

---
+++ src/step3.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class step3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
